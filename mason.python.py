2f330023ec46043633a9a9b445372b41b85c98e8
techcsmis <94011351+techcsmis@users.noreply.github.com>
2022-04-21 16:51:18 -0400

    Commit python file

---
+++ mason.python.py
@@ -0,0 +1,10 @@
+
+
+from xml.etree.ElementTree import Comment
+
+
+# mason.pathon.py file from gitrepos folder edureka.tutorial
+string1 = "Lay your burden on"
+stringL = "GOD."
+joinstringLstring1 = string1 + " " + stringL
+print(joinstringLstring1)
