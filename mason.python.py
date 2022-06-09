c5426a6e2672ad957ea051a070ad8d6faa2502ff
techcsmis <94011351+techcsmis@users.noreply.github.com>
2022-06-09 17:18:51 -0400

    commit of adds to remote repository github Git-DevOps-Repo

--- mason.python.py
+++ mason.python.py
@@ -1,10 +1,19 @@
 
 
+import string
+from tkinter.tix import INTEGER
 from xml.etree.ElementTree import Comment
 
 
 # mason.pathon.py file from gitrepos folder edureka.tutorial
+stringe = "8"
 string1 = "Lay your burden on"
 stringL = "GOD."
 joinstringLstring1 = string1 + " " + stringL
 print(joinstringLstring1)
+# skip line
+int5 = 7
+int3 = 11
+int8 = int5 + int3
+print(int8)
+
